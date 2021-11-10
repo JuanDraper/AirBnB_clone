2d34e9fb732807b46dff5d30842e720767774466
Andres Ortiz <3283@holbertonschool.com>
2021-11-10 19:21:12 +0000

    PEP8 compilance for console.py

--- console.py
+++ console.py
@@ -12,10 +12,11 @@ from models.review import Review
 from models.state import State
 from models.base_model import BaseModel
 
+
 class HBNBCommand(cmd.Cmd):
     """console class"""
 
-    prompt =  "(hbnb)"
+    prompt = "(hbnb)"
 
     def do_quit(self, args):
         """command that quits program"""
@@ -24,18 +25,18 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, args):
         """command that handles ctrl+D"""
         return True
-    
+
     def emptyline(self):
-        """makinig that an empty line  doesn´t execute anything"""
+        """making that an empty line doesn´t execute anything"""
         pass
-    
+
     def do_create(self, args):
         """command that creates instances of a given class"""
         args = shlex.split(args)
-        if args ==[]:
+        if args == []:
             print("**class name missing**")
-        elif args[0] not in ["BaseModel","User", "City", "Review", "Place",
-                            "State", "Amenity"]:
+        elif args[0] not in ["BaseModel", "User", "City", "Review", "Place",
+                             "State", "Amenity"]:
             print("**class doesn´t exist**")
         else:
             models.storage.reload()
@@ -46,10 +47,10 @@ class HBNBCommand(cmd.Cmd):
     def do_show(self, args):
         """prints the instance´s string representation"""
         args = shlex.split(args)
-        if args ==[]:
+        if args == []:
             print("**class name missing**")
-        elif args[0] not in ["BaseModel","User", "City", "Review", "Place",
-                            "State", "Amenity"]:
+        elif args[0] not in ["BaseModel", "User", "City", "Review", "Place",
+                             "State", "Amenity"]:
             print("**class doesn´t exist**")
         elif len(args) == 1:
             print("**instance id is missing**")
@@ -62,7 +63,7 @@ class HBNBCommand(cmd.Cmd):
             print("**no instance found**")
 
     def do_destroy(self, args):
-        """given an instance name and id, it will ve eliminated"""
+        """given an instance name and id, it will be eliminated"""
         args = shlex.split(args)
         if args == []:
             print("** class name missing **")
@@ -79,5 +80,6 @@ class HBNBCommand(cmd.Cmd):
                     del(instances[i])
                     return
             print("**no instance found**")
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
