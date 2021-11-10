50e4f6cca84deb57080484abbc93fd749502050c
Andres Ortiz <3283@holbertonschool.com>
2021-11-10 16:44:34 +0000

    Added classes to file_storage for serialization/deserialization

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,8 +1,15 @@
 import json
 from ..base_model import BaseModel
+from ..user import User
+from ..state import State
+from ..city import City
+from ..amenity import Amenity
+from ..place import Place
+from ..review import Review
 
 
-classes = { "BaseModel": BaseModel}
+classes = { "BaseModel": BaseModel, "User": User, "State": State, "City":
+            City, "Amenity": Amenity, "Place": Place, "Review": Review}
 
 class FileStorage:
     """serializes and deserializes json"""
